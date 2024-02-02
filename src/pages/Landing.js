142ba207d960707f70100b760c11c526310efa93
shweta228 <63063250+shweta228@users.noreply.github.com>
2024-02-03 01:08:50 +0530

    initial commit

---
+++ src/pages/Landing.js
@@ -0,0 +1,34 @@
+import React from "react";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/** info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            tempora facilis laboriosam quo fugit, doloribus deserunt nam
+            nesciunt? Excepturi, quae.
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
